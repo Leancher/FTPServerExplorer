227ffa2b9bc3b0f852b4ea28b6f0a68dc440c7e5
Andrew <leancher@yandex.ru>
2025-09-18 17:18:27 +0300

    Коментарии к коду

--- Program.cs
+++ Program.cs
@@ -1,10 +1,8 @@
-using Microsoft.AspNetCore.Components.Forms;
-using Microsoft.AspNetCore.SignalR;
-using System.IO;
 using System.Text.Json;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
-builder.Services.AddCors(); // добавляем сервисы CORS
+// Добавляем сервисы CORS
+builder.Services.AddCors(); 
 
 var app = builder.Build();
 
@@ -12,14 +10,15 @@ string[] dirsList;
 string curDir= "d:";
 string initFile = Environment.CurrentDirectory + "\\setup.ini";
 
-// настраиваем CORS
+await ReadInitFile();
+
+// Настраиваем CORS. Можно использовать все методы, заголовки, источники.
 app.UseCors(builder => builder.AllowAnyMethod().AllowAnyHeader().AllowAnyOrigin());
 
 app.Run(async (context) =>
 {
     var response = context.Response;
     var request = context.Request;
-    await ReadInitFile();
     await getSelectedDir(request, response);
 });
 
@@ -27,34 +26,48 @@ app.Run();
 
 async Task getSelectedDir(HttpRequest request, HttpResponse response)
 {
+    //При запросе с клиента данные записываются в Body
+    //Получаем в виде строки, затем преобразоваем в JSON
     using StreamReader reader = new(request.Body);
     string bodyAsString = await reader.ReadToEndAsync();
     Dir? dir = new("");
-    string json = JsonSerializer.Serialize(bodyAsString);
-    if (bodyAsString == "") await handleSelectedDir("/", response);
-    if (bodyAsString != null & bodyAsString != "")
+    string json = JsonSerializer.Serialize(bodyAsString);    
+    if (bodyAsString == "")
     {
-        dir = JsonSerializer.Deserialize<Dir>(bodyAsString);
-        await handleSelectedDir(dir?.Name, response);
+        //Если данных нет, значит нужна корневая папка
+        await handleSelectedDir("/", response);
+    }
+    else
+    {
+        //Если данные есть, извлекаем из JSON.
+        //Оператор ?? возвращает левый операнд, если этот операнд не равен null.
+        //Иначе возвращается правый операнд. При этом левый операнд должен принимать null.
+        dir = JsonSerializer.Deserialize<Dir>(bodyAsString ?? "/");
+        //? - если dir не равен null, то происходит обращение к его свойству Name
+        await handleSelectedDir(dir?.Name ?? "/", response);
     }
 }
-
+//Обработка полученной команды
 async Task handleSelectedDir(string selectedDir, HttpResponse response)
 {
     switch (selectedDir)
     {
+        //Происходит при первом запуске, отправляем страницу
         case "/":
             response.ContentType = "text/html; charset=utf-8";
             await response.SendFileAsync("index.html");
             break;
+        //После отправки старницы, она запрашивает корневую папку
         case "root":
             await sendDirsList(response, curDir);
             break;
+        //При нажатии кнопки Назад
         case "back":
             var len = curDir.LastIndexOf('/');
             if (len != -1) curDir = curDir.Substring(0, len);
             await sendDirsList(response, curDir);
             break;
+        //Во всех остальных случаях получаем название выбранной папки
         default:
             curDir = string.Concat(curDir, "/", selectedDir);
             await sendDirsList(response, curDir);
